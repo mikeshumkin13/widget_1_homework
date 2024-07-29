7c5c51a1a3008272db4189ed6bcebabb94851720
a12345 <shu.m2010@yandex.ru>
2024-07-29 14:41:21 +0300

    add tests for function and pytest

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+# Фикстура для предоставления примеров данных
+@pytest.fixture
+def sample_data():
+    return [
+        {"id": 1, "state": "EXECUTED", "date": "2023-07-29T12:34:56.789"},
+        {"id": 2, "state": "PENDING", "date": "2023-07-28T09:34:56.789"},
+        {"id": 3, "state": "EXECUTED", "date": "2023-07-27T05:34:56.789"},
+        {"id": 4, "state": "CANCELED", "date": "2023-07-26T03:34:56.789"},
+        {"id": 5, "state": "EXECUTED", "date": "2023-07-25T01:34:56.789"}
+    ]
+
+
+@pytest.fixture
+def mock_get_mask_account(mocker):
+    return mocker.patch('src.widget.get_mask_account', return_value='**7890')
+
+@pytest.fixture
+def mock_get_mask_card_number(mocker):
+    return mocker.patch('src.widget.get_mask_card_number', return_value='**** **** **** 5678')
+
+
---
+++ tests/test_processing.py
@@ -0,0 +1,24 @@
+import pytest
+from datetime import datetime
+from src.processing import filter_by_state, sort_by_date
+
+
+# Тест функции filter_by_state
+def test_filter_by_state(sample_data):
+    executed_operations = filter_by_state(sample_data)
+    assert len(executed_operations) == 3
+    assert all(op["state"] == "EXECUTED" for op in executed_operations)
+
+    pending_operations = filter_by_state(sample_data, state="PENDING")
+    assert len(pending_operations) == 1
+    assert pending_operations[0]["state"] == "PENDING"
+
+# Тест функции sort_by_date
+def test_sort_by_date(sample_data):
+    sorted_operations = sort_by_date(sample_data)
+    dates = [datetime.strptime(op["date"], "%Y-%m-%dT%H:%M:%S.%f") for op in sorted_operations]
+    assert dates == sorted(dates, reverse=True)
+
+    sorted_operations_asc = sort_by_date(sample_data, is_sort_reverse=False)
+    dates_asc = [datetime.strptime(op["date"], "%Y-%m-%dT%H:%M:%S.%f") for op in sorted_operations_asc]
+    assert dates_asc == sorted(dates_asc)
