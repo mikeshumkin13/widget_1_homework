75be9eda28a65a81fa1eba4b220130a1a2c516fd
a12345 <shu.m2010@yandex.ru>
2024-09-03 12:16:40 +0300

    add openpyxl

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,5 +1,10 @@
+import os
 import pandas as pd
 from typing import List, Dict, Any
+
+
+os.chdir('/Users/a12345/Documents/projects_for_skypro/widget_1')
+
 
 # Путь к CSV и Excel файлам в папке data
 csv_file_path = "data/transactions.csv"
@@ -16,3 +21,14 @@ def read_transactions_from_excel(file_path: str) -> List[Dict[str, Any]]:
     """Чтение финансовых операций из Excel-файла."""
     df = pd.read_excel(file_path)
     return df.to_dict(orient="records")
+
+
+
+transactions_from_csv = read_transactions_from_csv(csv_file_path)
+transactions_from_excel = read_transactions_from_excel(xls_file_path)
+
+print("Транзакции из CSV:")
+print(transactions_from_csv)
+
+print("Транзакции из Excel:")
+print(transactions_from_excel)
